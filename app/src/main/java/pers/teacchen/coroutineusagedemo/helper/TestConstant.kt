12df0e8c52bbc93d8cb952f7d4dcabc235620d5e
TeaC <carmidey@163.com>
2021-08-22 16:29:43 +0800

    Study: Complete cancel step one
    完成初遇取消部分内容

---
+++ app/src/main/java/pers/teacchen/coroutineusagedemo/helper/TestConstant.kt
@@ -0,0 +1,12 @@
+package pers.teacchen.coroutineusagedemo.helper
+
+/**
+ * @FileName: TestConstant
+ * @description:
+ *
+ * @author TeaC
+ * @date   2021-08-22
+ */
+
+const val FIVE_SECONDS = 5000L
+const val ONE_SECOND = 1000L
